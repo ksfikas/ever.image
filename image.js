f3c98b1c5194470655d708f36f3f24afba849e1e
Kostas <23658992+ksfikas@users.noreply.github.com>
2024-06-30 13:06:43 +0300

    add a canvas

--- image.js
+++ image.js
@@ -1,7 +1,42 @@
 function generate_binary_image(width, height, binary_number) {
-    var image = new Array[width];
+    var image = new Array(width);
 
     for (var i = 0; i < height; i++) {
         image[i] = new Array(height);
     }
+
+    var kostas = new ImageData(width,height);
+
+    var canvas = document.getElementById('canvas');
+    if (canvas != null)
+        canvas.remove();
+
+
+
+    canvas = document.createElement('canvas');
+
+    canvas.id = "CursorLayer";
+    canvas.width = width;
+    canvas.height = height;
+    canvas.style.zIndex = 8;
+    canvas.style.position = "absolute";
+    canvas.style.border = "1px solid";
+
+
+    var body = document.getElementsByTagName("body")[0];
+    body.appendChild(canvas);
+
+    cursorLayer = document.getElementById("CursorLayer");
+
+    console.log(cursorLayer);
+
+    // below is optional
+
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "rgba(255, 0, 0, 0.2)";
+    ctx.fillRect(100, 100, 200, 200);
+    ctx.fillStyle = "rgba(0, 255, 0, 0.2)";
+    ctx.fillRect(150, 150, 200, 200);
+    ctx.fillStyle = "rgba(0, 0, 255, 0.2)";
+    ctx.fillRect(200, 50, 200, 200);
 }
